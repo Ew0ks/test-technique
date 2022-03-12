739936854d9f9a4ac6143da7d50037985545f5e3
julien-gabard <juliengabard@hotmail.fr>
2022-03-12 15:25:07 +0100

    delete useRef

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -8,7 +8,6 @@ import "./Map.scss";
 // == Component
 const Map = () => {
     const mapContainer = useRef(null);
-    const map = useRef(null);
     const [API_KEY] = useState("4qgA42jnxrnWDigecejN");
     const [viewport] = useState({
         latitude: 46.8,
@@ -18,18 +17,14 @@ const Map = () => {
 
     useEffect(() => {
 
-        if (map.current) {
-            return;
-        }
-
-        map.current = new maplibregl.Map({
+        new maplibregl.Map({
             container: mapContainer.current,
             style: `https://api.maptiler.com/maps/streets/style.json?key=${API_KEY}`,
             center: [viewport.longitude, viewport.latitude],
             zoom: viewport.zoom
         });
 
-    }, [map.current]);
+    }, []);
 
     return (
         <div className="map-contained">
