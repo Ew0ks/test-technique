4affb8816fbd616a3f066ce5c9d9ade954ee5f64
julien-gabard <juliengabard@hotmail.fr>
2022-03-13 15:42:06 +0100

    Use only maplibre-gl in ViewMap

--- src/components/ViewMap/index.jsx
+++ src/components/ViewMap/index.jsx
@@ -1,27 +1,45 @@
 // == Import npm
-import { useState } from "react";
-import ReactGl from "react-map-gl";
+import { useEffect, useRef, useState } from "react";
 import mapLibreGl from "maplibre-gl";
 
 // == Import style
-import "./Map.scss";
+import "./ViewMap.scss";
 
 // == Component
 const ViewMap = () => {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [API_KEY] = useState('4qgA42jnxrnWDigecejN');
     const [viewState] = useState({
         longitude: 2.2,
         latitude: 46.8,
         zoom: 5
     });
 
+    mapLibreGl.prewarm();
+    mapLibreGl.workerUrl = `https://api.maptiler.com/maps/streets/style.json?key=${API_KEY}`;
+
+    useEffect(() => {
+
+        if (!mapLibreGl.supported()) {
+            alert('Your browser does not support MapLibre GL');
+        }
+
+        if (map.current) {
+            return;
+        }
+
+        map.current = new mapLibreGl.Map({
+            container: mapContainer.current,
+            style: mapLibreGl.workerUrl,
+            center: [viewState.longitude, viewState.latitude],
+            zoom: viewState.zoom
+        });
+    });
+
     return (
         <div className="map-wrap">
-            <ReactGl
-                reuseMaps={true} // Stores the board to avoid being destroyed when disassembling the component
-                initialViewState={{...viewState}}
-                mapLib={mapLibreGl}
-                mapStyle={"https://demotiles.maplibre.org/style.json"}
-            />
+            <div ref={mapContainer} className="map" />
         </div>
     );
 };
